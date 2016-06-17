c68e54a4017ae17d5644bc38ac875c1e2e156d9f
JananiBhagavathi <bhagavathi.janani11@gmail.com>
2016-06-17 12:28:00 +0530

    Create gre.c

---
+++ gre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c;
+   scanf("%d %d %d",&a,&b,&c);
+   if((a>b)&&(a>c))
+   printf("%d is greater than %d %d",a,b,c);
+   else if((b>a)&&(b>c))
+   printf("%d is greater than %d %d",b,a,c);
+   else
+   printf("%d is greater than %d %d",c,a,b);
+    return 0;
+}
